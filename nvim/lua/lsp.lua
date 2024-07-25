a67135ad873d0b7e9746dc232316704930a816ef
Andres <andresmargar98@gmail.com>
2024-07-25 17:48:49 +0200

    python lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -8,9 +8,44 @@ require("mason").setup()
 require('mason-lspconfig').setup({
   ensure_installed = {},
   handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
+      function(server_name)
+          require('lspconfig')[server_name].setup({})
+      end,
+      rust_analyzer = function()
+          require('lspconfig').rust_analyzer.setup({
+              settings = {
+                  ["rust-analyzer"] = {
+                      imports = {
+                          granularity = {
+                              group = "module",
+                          },
+                          prefix = "self",
+                      },
+                      cargo = {
+                          buildScripts = {
+                              enable = true,
+                          },
+                      },
+                      procMacro = {
+                          enable = true
+                      },
+                  }
+              }
+          })
+      end,
+      pylsp = function()
+        require("lspconfig").pylsp.setup({
+            settings = {
+                pylsp = {
+                    plugins = {
+                        pycodestyle = { enabled = false },
+                        flake8 = { enabled = false },
+                        pylint = { enabled = false }
+                    }
+                }
+            }
+        })
+      end,
   },
 })
 
