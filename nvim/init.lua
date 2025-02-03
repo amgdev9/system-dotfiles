2ed2591b95921f41d09d1b9e963b9f7c3e6b242c
Andrés Martínez <andres.martinez@stay-app.com>
2025-02-03 10:55:51 +0100

    remove plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,3 @@
-local safe_require = require("utils").safe_require
-local flags = safe_require("flags")
-
 vim.g.loaded_netrw = 1	-- Using nvim-tree as a file explorer
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
@@ -16,12 +13,3 @@ require("nvim_tree")
 require("color-scheme")
 require("status-bar")
 require("finder")
-
-if flags ~= nil and flags.flutter then
-    require("flutter")
-end
-
-if flags ~= nil and flags.xcode then
-    require("xcode")
-end
-
