ddb86b958460dc593f3c16ec6efb098c31ed7ec0
AMG <andresmargar98@proton.me>
2025-08-31 16:46:32 +0200

    refactor

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 LOCATION=$HOME/.config/terminal-config
 
-[ ! -e $HOME/Scripts ] && ln -s $HOME/.config/terminal-config/scripts/ $HOME/Scripts
-[ ! -e $HOME/.config/alacritty ] && ln -s $HOME/.config/terminal-config/alacritty $HOME/.config/alacritty
-[ ! -e $HOME/.config/hypr ] && ln -s $HOME/.config/terminal-config/hypr $HOME/.config/hypr
-[ ! -e $HOME/.config/nvim ] && ln -s $HOME/.config/terminal-config/nvim $HOME/.config/nvim
-[ ! -e $HOME/.config/nvim-code ] && ln -s $HOME/.config/terminal-config/nvim-code $HOME/.config/nvim-code
-[ ! -e $HOME/.config/nvim-debugger ] && ln -s $HOME/.config/terminal-config/nvim-debugger $HOME/.config/nvim-debugger
-[ ! -e $HOME/.config/tmux ] && ln -s $HOME/.config/terminal-config/tmux $HOME/.config/tmux
-[ ! -e $HOME/.config/waybar ] && ln -s $HOME/.config/terminal-config/waybar $HOME/.config/waybar
-[ ! -e $HOME/.config/wofi ] && ln -s $HOME/.config/terminal-config/wofi $HOME/.config/wofi
+[ ! -e $HOME/Scripts ] && ln -s $LOCATION/scripts/ $HOME/Scripts
+[ ! -e $HOME/.config/alacritty ] && ln -s $LOCATION/alacritty $HOME/.config/alacritty
+[ ! -e $HOME/.config/hypr ] && ln -s $LOCATION/hypr $HOME/.config/hypr
+[ ! -e $HOME/.config/nvim ] && ln -s $LOCATION/nvim $HOME/.config/nvim
+[ ! -e $HOME/.config/nvim-code ] && ln -s $LOCATION/nvim-code $HOME/.config/nvim-code
+[ ! -e $HOME/.config/nvim-debugger ] && ln -s $LOCATION/nvim-debugger $HOME/.config/nvim-debugger
+[ ! -e $HOME/.config/tmux ] && ln -s $LOCATION/tmux $HOME/.config/tmux
+[ ! -e $HOME/.config/waybar ] && ln -s $LOCATION/waybar $HOME/.config/waybar
+[ ! -e $HOME/.config/wofi ] && ln -s $LOCATION/wofi $HOME/.config/wofi
 
 rm -f $HOME/.bashrc
-ln -s $HOME/.config/terminal-config/home/.bashrc $HOME/.bashrc
+ln -s $LOCATION/home/.bashrc $HOME/.bashrc
 
 rm -f $HOME/.bash_profile
-ln -s $HOME/.config/terminal-config/home/.bash_profile $HOME/.bash_profile
+ln -s $LOCATION/home/.bash_profile $HOME/.bash_profile
 
 rm -f $HOME/.inputrc
-ln -s $HOME/.config/terminal-config/home/.inputrc $HOME/.inputrc
+ln -s $LOCATION/home/.inputrc $HOME/.inputrc
 
 git config --global user.email "andresmargar98@proton.me"
 git config --global user.name "AMG"
 git config --global init.defaultBranch main
-git -C $HOME/.config/terminal-config submodule update --init --recursive
+git -C $LOCATION submodule update --init --recursive
 
-sudo cp $HOME/.config/terminal-config/home/.bashrc_root /root/.bashrc
+sudo cp $LOCATION/home/.bashrc_root /root/.bashrc
 
 mkdir -p $HOME/Projects
 
