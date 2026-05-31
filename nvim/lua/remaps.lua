7dfaed6ca3638edd5211bffee9813a851ac34908
AMG <andresmargar98@proton.me>
2026-05-31 19:00:31 +0200

    netrw

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -1,3 +1,11 @@
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>e", function()
+  if vim.bo.filetype == "netrw" then
+    vim.cmd("Rexplore")
+  else
+    vim.cmd("Explore")
+  end
+end)
